58a44b01b876ab4986f0b940f25abe18c717696a
Dyego Maas <dyego.maas@gmail.com>
2013-11-03 16:23:01 -0200

    DM:12: Implementada versão inicial do Sequenciador

--- Assets/Scripts/Sequenciador/Sequenciador.cs
+++ Assets/Scripts/Sequenciador/Sequenciador.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,8 @@ public class Sequenciador : MonoBehaviour
 {
     private GeradorAtaques geradorAtaques;
     private readonly List<Ataque> ataques = new List<Ataque>();
+
+    const int DuracaoAtaque = 1;
 
     // Use this for initialization
 	void Start ()
@@ -37,11 +40,32 @@ public class Sequenciador : MonoBehaviour
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(0, 0, 50, 50), "Gerar ataque"))
+        if (GUI.Button(new Rect(0, 0, 100, 50), "Gerar ataque"))
         {
             var ataque = geradorAtaques.GerarAtaque();
             ataques.Add(ataque);
             Debug.Log(ataque);
         }
+
+        if (GUI.Button(new Rect(0, 100, 100, 50), "Reproduzir Sequência"))
+        {
+            StartCoroutine(ReproduzirSequenciaAtaques());
+        }
+    }
+
+    private IEnumerator ReproduzirSequenciaAtaques()
+    {
+        Debug.Log("-----");
+        Debug.Log("Sequência de ataques:");
+        foreach (var ataque in ataques)
+        {
+            ReproduzirAtaque(ataque);
+            yield return new WaitForSeconds(DuracaoAtaque);
+        }
+    }
+
+    private void ReproduzirAtaque(Ataque ataque)
+    {
+        Debug.Log(ataque);
     }
 }
