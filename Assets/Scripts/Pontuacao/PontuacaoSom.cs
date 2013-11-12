2431566a01ef626fde2b81c2e9dc53e1f1a3dad3
Dyego Maas <dyego.maas@gmail.com>
2013-11-11 22:16:30 -0200

    DM:21: Adicionado som ao ganhar moedas

---
+++ Assets/Scripts/Pontuacao/PontuacaoSom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PontuacaoSom : MonoBehaviour
+{
+
+    public AudioClip somMoeda;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void AtualizarPontuacao()
+    {
+        if(somMoeda)
+            AudioSource.PlayClipAtPoint(somMoeda, transform.position);
+    }
+}
