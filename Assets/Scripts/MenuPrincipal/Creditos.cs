c117fcbe0693060c9e4fc2c872cd32615960faa5
Dyego Maas <dyego.maas@gmail.com>
2013-11-26 22:49:08 -0200

    DM:33: Criada tela de créditos

---
+++ Assets/Scripts/MenuPrincipal/Creditos.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Creditos : MonoBehaviour
+{
+    private float positcaoInicalY;
+    private bool visivel;
+
+    // Use this for initialization
+    void Start()
+    {
+        positcaoInicalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AbrirClick()
+    {
+        if (!visivel)
+        {
+            Show();
+            visivel = true;
+        }
+    }
+
+    void FecharClick()
+    {
+        if (visivel)
+        {
+            Hide();
+            visivel = false;
+        }
+    }
+
+    private void Hide()
+    {
+        var posicaoFinal = new Vector3(transform.position.x, positcaoInicalY, transform.position.z);
+        iTween.MoveTo(gameObject, iTween.Hash("position", posicaoFinal, "time", .4f, "easetype", iTween.EaseType.easeOutCirc, "islocal", true));
+    }
+
+    private void Show()
+    {
+        var posicaoFinal = new Vector3(transform.position.x, 0, transform.position.z);
+        iTween.MoveTo(gameObject, iTween.Hash("position", posicaoFinal, "time", .4f, "easetype", iTween.EaseType.easeOutCirc, "islocal", true));
+    }
+}
