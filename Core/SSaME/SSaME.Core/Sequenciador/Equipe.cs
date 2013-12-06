43d9cdac0fc503edf76165bfff6dd6ac59daab21
Dyego Maas <dyego.maas@gmail.com>
2013-12-05 23:28:04 -0200

    DM:27: Reorganização dos projetos do Core + removida solução antiga.

--- Core/SSaME/SSaME.Core/Sequenciador/Equipe.cs
+++
@@ -1,8 +0,0 @@
-namespace SSaME.Core.Sequenciador
-{
-    public enum Equipe
-    {
-        A = 1,
-        B = 2
-    }
-}
--- Core/SSaME/SSaME.Core/Sequenciador/IRandom.cs
+++
@@ -1,8 +0,0 @@
-namespace SSaME.Core.Sequenciador
-{
-    public interface IRandom  
-    {
-        bool Bool();
-        int Range(int valorMinimo, int valorMaximo);
-    }
-}
