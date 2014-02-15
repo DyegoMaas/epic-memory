1c558a6a3f988e376be0c331b583c00ca594f160
Dyego Maas <dyego.maas@gmail.com>
2014-02-15 15:55:04 -0200

    DM: criado novo GUI para o indicador de jogador ativo, que agora é um objeto que gira 180º no eixo x cada vez que o jogador ativo muda.

---
+++ Assets/Scripts/UI/Gira180.cs
@@ -0,0 +1,35 @@
+﻿using Messaging;
+using UnityEngine;
+using System.Collections;
+
+public class Gira180 : MonoBehaviour {
+
+    public iTween.EaseType EaseType = iTween.EaseType.easeOutCubic;
+    public float duracaoGiro = .5f;
+    public PerfilJogadorAtivo perfilAtivo = PerfilJogadorAtivo.Maquina;
+
+	// Use this for initialization
+	void Start () {
+        Messenger.Subscribe(MessageType.PerfilJogadorAtivado, gameObject, "VerificarGiro");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    void VerificarGiro(Message<PerfilJogadorAtivo> mensagem)
+    {
+        if (mensagem.Value != perfilAtivo)
+        {
+            perfilAtivo = mensagem.Value;
+            Girar180();
+        }
+    }
+    
+    private void Girar180()
+    {
+        if(gameObject)
+            iTween.RotateBy(gameObject, iTween.Hash("x", .5f, "time", duracaoGiro, "easetype", EaseType, "islocal", true));
+    }
+}
